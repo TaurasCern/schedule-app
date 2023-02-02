36c3e7840f3ac1c581c3bdada289a49d2538de9f
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-02-02 17:54:09 +0200

    created navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Header</h1>
+</template>
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        name: `PageHeader`,
+        mounted() {
+            let user = localStorage.getItem(`user`);
+        }
+    }) 
+</script>
