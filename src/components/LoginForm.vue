3a7f1564b26e079e0c6c42caffaf358ee4b37c2d
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-02-02 17:55:42 +0200

    created placeholder login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,28 @@
+<template>
+    <form @submit.prevent="login">
+        <input type="text" v-model="email">
+        <input type="password" v-model="password">
+        <input type="button" @click="login()" value="Log in">
+    </form>
+</template>
+<script lang="ts">
+    import { defineComponent, ref } from 'vue'
+
+    export default defineComponent({
+        name: `LoginForm`,
+        components: {},
+        setup() {
+            const email = ref<string>(``);
+            const password = ref<string>(``);
+
+            const login = async () => {
+                console.log(password.value);
+            }
+            return { login, email, password }
+        }
+    })
+</script>
+
+<style scoped>
+
+</style>
