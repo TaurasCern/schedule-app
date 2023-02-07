602b4ba1d2b7d5595e9320ab7e9129f3a2fa340a
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-02-07 18:35:26 +0200

    Added Home component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <NavBar/>
+  <NavBar :key="$route.fullPath"/>
   <router-view/>
 </template>
 <script lang="ts">
@@ -18,18 +18,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <article>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium enim eveniet similique praesentium incidunt facilis consequuntur ratione obcaecati? Atque recusandae inventore dolores ipsa explicabo doloremque earum blanditiis nemo ea odit.
+        </div>
+    </article>
+</template>
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        name: `LoginForm`,
+        components: {},
+    })
+</script>
+
+<style scoped>
+
+</style>
