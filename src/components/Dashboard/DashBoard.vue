0ce744078edfc732b34eac94aab3a1d5995f15f9
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-03-12 18:10:40 +0200

    created dashboard

---
+++ src/components/Dashboard/DashBoard.vue
@@ -0,0 +1,26 @@
+<template>
+    <article>
+        <ScheduleBlock/>
+        <ScheduleBlock/>
+
+    </article>
+</template>
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import ScheduleBlock from './ScheduleNotes/ScheduleBlock.vue'
+
+
+    export default defineComponent({
+        name: `Dashboard`,
+        components: { ScheduleBlock },
+    })
+</script>
+<style scoped>
+    article {
+        display: flex;
+        flex-flow: row wrap;
+        justify-items: flex-start;
+        width: 80%;
+        margin: 0 auto;
+    }
+</style>
