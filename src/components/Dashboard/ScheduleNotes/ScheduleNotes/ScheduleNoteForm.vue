b04450add63488329808ff2609a6ba8c5fbd226c
Taurenxoo <51359787+Taurenxoo@users.noreply.github.com>
2023-03-17 01:59:38 +0200

    fixed time formatting

--- src/components/Dashboard/ScheduleNotes/ScheduleNotes/ScheduleNoteForm.vue
+++ src/components/Dashboard/ScheduleNotes/ScheduleNotes/ScheduleNoteForm.vue
@@ -1,16 +1,15 @@
 <template>
-    <form class="note-form-container" @submit.prevent="postNote">
-        <b>New note</b>
+    <form class="note-form-container">
         <div class="form-input" id="note-input">
-            <label>Note:</label>
-            <textarea v-model="note" cols="30" rows="10"></textarea>
+            <label><b>New note</b></label>
+            <textarea v-model="note" cols="30" rows="10" maxlength="150"></textarea>
         </div>
-        <div class="form-input">
-            <label>From:</label>
+        <div class="date" id="end">
+            <label><b>From:</b></label>
             <input type="datetime-local" v-model="from">
         </div>
-        <div class="form-input">
-            <label>To:</label>
+        <div class="date">
+            <label><b>To:</b></label>
             <input type="datetime-local" v-model="to">
         </div>
         <input type="button" @click="postNote()" value="Add note">
@@ -26,18 +25,38 @@
 
         data() {
             return {
-                from: (new Date()).toISOString().substring(0,16),
-                to: (new Date()).toISOString().substring(0,16),
+                from: this.getFromDate() as string | Date,
+                to: this.getToDate() as string | Date,
                 note: `` as string,
                 notes: [] as ScheduleNote[],
             }
         },
         methods: {
+            getFromDate() {
+                let date = new Date();
+                let month = date.getMonth().toString();
+                let day = date.getDay().toString();
+                let hour = date.getHours().toString();
+                let minutes = date.getMinutes().toString();
+
+                return `${date.getFullYear()}-${month.length < 2 ? `0${month}` : month}-${day.length < 2 ? `0${day}` : day}` +
+                `T${hour.length < 2 ? `0${hour}` : hour}:${minutes.length < 2 ? `0${minutes}` : minutes}`;
+            },
+            getToDate() {
+                let date = new Date();
+                let minutes = (date.getMinutes() + 15) > 59 ? `59` : (date.getMinutes() + 15).toString();
+                let hour = date.getHours().toString();
+                let day = date.getDay().toString();
+                let month = date.getMonth().toString();
+
+                return `${date.getFullYear()}-${month.length < 2 ? `0${month}` : month}-`+
+                `${day.length < 2 ? `0${day}` : day}T${hour.length < 2 ? `0${hour}` : hour}:${minutes}`;
+            },
             async postNote() {
                 let scheduleNote: ScheduleNoteRequest = {
                     from: new Date(this.$data.from),
                     to: new Date(this.$data.to),
-                    note: this.$data.note
+                    note: this.$data.note as string
                 }; 
                 let response = await fetch(`http://localhost:5208/api/ScheduledTime`, {
                     method: `POST`,
@@ -52,8 +71,7 @@
                     this.$emit(`posted`, await response.json())
                 }
                 else {
-                    console.log(response.status);
-                    
+                    console.log(response.status);                  
                 }
             }
         }
@@ -67,6 +85,7 @@
         width: 280px;
         height: 160px;
         margin: 10px;
+        padding: 10px;
     }
     textarea {
         resize: none;
@@ -77,5 +96,15 @@
         display: flex;
         flex-flow: column;
         margin: 0 auto;
+    }
+    .date {
+        display: inline-flex;
+    }
+    .date label {
+        display: inline-flex;
+        width: 40%;
+    }
+    #end {
+        margin-top: auto;
     }
 </style>
